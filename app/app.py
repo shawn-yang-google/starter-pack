186e34b42241330dba29efe8b78e33f699583c71
shawn-yang-google <xiaolongyang@google.com>
2025-01-10 15:17:48 -0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,5 +3,4 @@ from vertexai.preview import reasoning_engines
 def create_agent():
     return reasoning_engines.LangchainAgent(
         model="gemini-1.0-pro-001",
-        tools=[get_exchange_rate],
     )
